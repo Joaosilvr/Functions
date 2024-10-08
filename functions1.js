7a5da0cb4e3c8e0ba080bc342f6d31d5198a82aa
joaosilvr <joaovitor.dasilvaramos@gmail.com>
2024-10-08 19:09:45 -0300

    comitando

---
+++ functions1.js
@@ -0,0 +1,12 @@
+// exercicio 1
+
+function printarOlaMundo(mensagem) {
+    console.log(mensagem);
+    
+}
+printarOlaMundo("Olá mundo!!")
+
+function printarOlaMundo2() {
+    console.log('Olá Mundo!');
+}
+printarOlaMundo2()
---
+++ functions2.js
@@ -0,0 +1,12 @@
+function receberNome(nome) {
+    console.log(`Olá ${nome}.`);
+}
+receberNome('João')
+receberNome('Guilherme')
+receberNome('Will') 
+
+function calcularArea(altura,largura) {
+    const area = altura * largura
+    return area
+}
+calcularArea(2,3)
